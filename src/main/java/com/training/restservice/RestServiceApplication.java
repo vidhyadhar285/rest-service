97c8d1c6f1ff82d71ba21065c883231bcea94057
vidhyadhar <vidhya2014n@gmail.com>
2020-03-15 08:35:48 +0530

    First commit

---
+++ src/main/java/com/training/restservice/RestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.training.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceApplication.class, args);
+	}
+
+}
